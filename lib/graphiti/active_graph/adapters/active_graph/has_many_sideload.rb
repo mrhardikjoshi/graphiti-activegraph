b138d07662172bf9c7cd84e190591eee97879674
Hardik Joshi <hardikjoshi1991@gmail.com>
2020-07-02 00:19:13 +0530

    updated adapter class

--- lib/graphiti/active_graph/adapters/active_graph/has_many_sideload.rb
+++ lib/graphiti/active_graph/adapters/active_graph/has_many_sideload.rb
@@ -1,4 +1,7 @@
+require "graphiti/active_graph/adapters/active_graph/sideload"
+
 class Graphiti::ActiveGraph::Adapters::ActiveGraph::HasManySideload < Graphiti::Sideload::HasMany
+  include Graphiti::ActiveGraph::Adapters::ActiveGraph::Sideload
   def default_base_scope
     resource_class.model.all
   end
