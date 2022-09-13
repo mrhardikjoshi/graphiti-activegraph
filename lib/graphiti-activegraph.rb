1bdcf0fe6c4012eee4b532eac91c4fc42dcb44e7
Heinrich Klobuczek <heinrich@mail.com>
2022-09-13 05:39:07 +0200

    Avoid Zeitwerk warnings (#14)
    * Avoid zeitwerk warnings
    
    * bump version
    
    * zeitwerk fixes
    
    * travis build
    
    * ruby version requirement
    
    * removed ruby version requirement
    
    * zeitwerk do not warn on extra files
    
    * fixed couple of bugs in a minimal way
    
    Co-authored-by: Hardik Joshi <hardikjoshi1991@gmail.com>

--- lib/graphiti-activegraph.rb
+++ lib/graphiti-activegraph.rb
@@ -1,31 +1,17 @@
 require 'zeitwerk'
 
-# Workaround for missing zeitwerk support as of jruby-9.2.13.0
+# Workaround for jruby prepend issue https://github.com/jruby/jruby/issues/6971
 module Graphiti
+  module ActiveGraph
+  end
   module Scoping
   end
-  module ActiveGraph
-    module Scoping
-    end
-    module Adapters
-    end
-    module Util
-    end
-    module JsonapiExt
-      module Serializable
-      end
-    end
-  end
 end
-# End workaround
-
-# Workaround for jruby prepend issue https://github.com/jruby/jruby/issues/6971
 require 'graphiti/scoping/filterable'
 require 'graphiti/resource/persistence'
 # End workaround for jruby prepend issue
 
-loader = Zeitwerk::Loader.for_gem
-loader.inflector.inflect 'version' => 'VERSION'
+loader = Zeitwerk::Loader.for_gem(warn_on_extra_files: false)
 loader.ignore(File.expand_path('graphiti-activegraph.rb', __dir__))
 loader.setup
 
--- lib/graphiti/active_graph/version.rb
+++ lib/graphiti/active_graph/version.rb
@@ -1,5 +1,5 @@
 module Graphiti
   module ActiveGraph
-    VERSION = '0.1.15'
+    VERSION = '0.1.16'
   end
 end
