477ac4059d8a2ebd296a2965db4bf0417916405c
lkalwa <leszek.kalwa@gmail.com>
2022-11-10 16:43:03 +0530

    Clean up unecessary changes

--- lib/graphiti/active_graph/runner.rb
+++ lib/graphiti/active_graph/runner.rb
@@ -16,13 +16,12 @@ module Graphiti::ActiveGraph
     def proxy(base = nil, opts = {})
       base ||= jsonapi_resource.base_scope
       scope_opts = opts.slice(:sideload_parent_length,
-                              :default_paginate,
-                              :after_resolve,
-                              :sideload,
-                              :parent,
-                              :params,
-                              :preloaded,
-                              :skip_scoping).merge(unpaginated_query: params[:unpaginated_query])
+        :default_paginate,
+        :after_resolve,
+        :sideload,
+        :parent,
+        :params,
+        :preloaded).merge(unpaginated_query: params[:unpaginated_query])
       scope = jsonapi_scope(base, scope_opts)
       preloaded = opts[:preloaded]
       options = { payload: deserialized_payload,
