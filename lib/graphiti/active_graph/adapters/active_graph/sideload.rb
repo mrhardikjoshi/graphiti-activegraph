71ccb5baadc2c211840effd8becf5515e8ab8ae9
Hardik Joshi <hardikjoshi1991@gmail.com>
2024-04-29 18:24:07 +0530

    polymorphic assoc flag

--- lib/graphiti/active_graph/adapters/active_graph/sideload.rb
+++ lib/graphiti/active_graph/adapters/active_graph/sideload.rb
@@ -12,57 +12,15 @@ module Graphiti::ActiveGraph::Adapters::ActiveGraph::Sideload
     end
   end
 
-  # def base_scope
-  #   if @base_scope
-  #     @base_scope.respond_to?(:call) ? @base_scope.call : @base_scope
-  #   else
-  #     resource.base_scope
-  #   end
-  # end
+  def default_base_scope
+    resource_class.model.all
+  end
 
-  # def load(parents, query, graph_parent)
-  #   params, opts, proxy = nil, nil, nil
+  def infer_foreign_key
+    association_name.to_sym
+  end
 
-  #   with_error_handling Errors::SideloadParamsError do
-  #     params = load_params(parents, query)
-  #     params_proc&.call(params, parents, context)
-  #     return [] if blank_query?(params)
-  #     opts = load_options(parents, query)
-  #     opts[:sideload] = self
-  #     opts[:parent] = graph_parent
-  #   end
-
-  #   with_error_handling(Errors::SideloadQueryBuildingError) do
-  #     proxy = resource.class._all(params, opts, base_scope)
-  #     pre_load_proc&.call(proxy, parents)
-  #   end
-
-  #   proxy.to_a
-  # end
-
-  # def load(parents, _, _)
-  #   child_arr = []
-  #   @child_map = resource_with_parent_assoc(parents).each_with_object({}) do |arr, hash|
-  #     child_obj = arr.first
-  #     parent_uuid = arr.last
-  #     hash[parent_uuid] ||= []
-  #     hash[parent_uuid] << child_obj
-  #     child_arr << child_obj
-  #   end
-
-  #   fire_assign(parents, child_arr)
-  #   child_arr
-  # end
-
-  # def resource_with_parent_assoc(parents)
-  #   parent_ids = parents.pluck(:id)
-  #   proxy = parent_resource_class.model.as(:p).where(id: parent_ids)
-  #   scope = self.class.sideload_scope
-
-  #   if scope.present?
-  #     scope.call(proxy)
-  #   else
-  #     proxy.send(association_name, :children)
-  #   end.query.pluck(:children, p: :neo_id)
-  # end
+  def polymorphic?
+    false
+  end
 end
